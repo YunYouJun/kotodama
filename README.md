3d3532d2f77afcb107b9b098f58db26bba5c0747
YunYouJun <me@yunyoujun.cn>
2021-10-25 21:36:49 +0800

    feat: init basic structure

---
+++ README.md
@@ -0,0 +1,9 @@
+# kotodama
+
+> 💬 掌控言灵的魔法，一个优雅的纯静态评论管理后台
+
+- ⚡️ [Vue 3](https://github.com/vuejs/vue-next), [Vite 2](https://github.com/vitejs/vite), [pnpm](https://pnpm.js.org/), [element-plus](https://github.com/element-plus/element-plus) - born for future
+
+## Waline
+
+适配 [Waline](https://github.com/walinejs/waline) 后台管理
---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+// https://github.com/vueuse/head
+// you can use this to manipulate the document head in any components,
+// they will be rendered correctly in the html results with vite-ssg
+useHead({
+  title: 'Kotodama 言灵',
+  meta: [
+    { name: 'description', content: '一个优雅的纯静态评论管理后台' },
+  ],
+})
+</script>
+
+<template>
+  <router-view />
+</template>
