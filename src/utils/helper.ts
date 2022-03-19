7ace4a2c42ae161d867fc8427e840b277146bdf8
YunYouJun <me@yunyoujun.cn>
2022-03-19 19:20:19 +0800

    fix: adapt for hash email address

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,12 +1,14 @@
 import md5 from 'md5'
 
+const avatarCDN = 'https://sdn.geekzu.org/avatar/'
+
 /**
  * 根据邮件地址获取头像
  * @param email
  * @returns
  */
 export function getAvatarUrl(email: string, size = 40) {
-  return `https://sdn.geekzu.org/avatar/${md5(email)}?s=${size}&r=G&d=`
+  return `${avatarCDN}${email.includes('@') ? md5(email) : email}?s=${size}&r=G&d=`
 }
 
 /**
