221f43185621564027e1a9b47cf637cbcc5ac2f5
YunYouJun <me@yunyoujun.cn>
2022-07-20 23:17:19 +0800

    feat: use webHashHistory

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@
 import { ViteSSG } from 'vite-ssg'
 import generatedRoutes from 'virtual:generated-pages'
 import { setupLayouts } from 'virtual:generated-layouts'
+import { createWebHashHistory } from 'vue-router'
 import App from './App.vue'
 
 // import '@unocss/reset/tailwind.css'
@@ -21,7 +22,7 @@ const routes = setupLayouts(generatedRoutes)
 // https://github.com/antfu/vite-ssg
 export const createApp = ViteSSG(
   App,
-  { routes },
+  { history: createWebHashHistory(), routes },
   (ctx) => {
     // install all modules under `modules/`
     Object.values(import.meta.globEager('./modules/*.ts')).map(i => i.install?.(ctx))
