90ca8febe39fbb0f26835cca154c508d1c284456
YunYouJun <me@yunyoujun.cn>
2022-10-04 23:37:35 +0800

    feat: show version in footer

--- src/components/base/BaseFooter.vue
+++ src/components/base/BaseFooter.vue
@@ -3,9 +3,14 @@ import pkg from '~/../package.json'
 </script>
 
 <template>
-  <div class="my-5 mx-auto text-center opacity-60 text-sm">
-    <a :href="pkg.repository.url" target="_blank">Kotodama</a>
-    <a class="mx-2" href="https://sponsors.yunyoujun.cn" target="_blank">☁️</a>
-    <a href="https://github.com/YunYouJun" target="_blank">@YunYouJun</a>
+  <div class="mt-6 mx-auto text-center opacity-70 text-sm">
+    <div>
+      <a :href="pkg.repository.url" target="_blank">Kotodama</a>
+      <a class="mx-2" href="https://sponsors.yunyoujun.cn" target="_blank">☁️</a>
+      <a href="https://github.com/YunYouJun" target="_blank">@YunYouJun</a>
+    </div>
+    <div m="y-2">
+      v{{ pkg.version }}
+    </div>
   </div>
 </template>
